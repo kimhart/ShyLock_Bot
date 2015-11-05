fda67822a6c548f752f7da2cfccce30af02f0ce7
Kim Hart <kimhart90@gmail.com>
2015-11-05 00:14:34 -0500

    major shylock improvements

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
-$(document).ready(function() {
-console.log('linked');
+$(document).ready(function(){ 
 
 
+// The indentation/organization of this code is heinous and I apologize! 
 
+
+var purse = 10000
+var $log = $('#log');
+var $prompt = $('#prompt').val();
+var $logContainer = $('#logContainer');
+
+var handleInput = function(event){
+	var $prompt = $('#prompt').val();
+	
+	if(event.keyCode===13){
+	
+		var $li = $('<li>');
+			if ($prompt === 'purse') {
+				$log.append($li);
+				$li.text('Yo! I\'m Shylock and I have ' + purse + ' ducats up for grabs.');
+				$('#prompt').val("");
+	} else if ($prompt.split(' ')[0] === 'loan' || 'Loan') {
+		var customerName = $prompt.split(' ')[1];
+		var loanAmount = $prompt.split(' ')[2];
+		var ledger = purse - loanAmount;
+				$log.append($li);
+				$li.text(customerName + ' now owes me ' + loanAmount + ' ducats. New ledger: ' + ledger + ' ducats.');
+				$('#prompt').val(""); // should make a sequence that makes it so you can't borrow more than 10,000 ducats...but ran out of time
+	} else if ($prompt) {
+				$log.append($li);
+				$li.text('Yikes... that\'s not a legit request. Try again.');
+				$('#prompt').val("");
+	}
+}
+};
+
+$(document).on('keypress', handleInput);
 
 });
+ 
